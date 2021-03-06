1660cecd9c2ffb2584bb9e505b28bf5689b8ad2a
Matt Shimizu <mattmshimizu@gmail.com>
2021-03-05 16:45:16 -0800

    Add database operations to CoreServices
    Simple CRUD operations reaching into the DbContext, summaries are a bit
    overkill here, but they would have a bit more substance with some more
    interesting methods. May return here later for more error handling and
    unhappy path.

--- Server/Core.API/CoreService.cs
+++ Server/Core.API/CoreService.cs
@@ -19,33 +19,61 @@ namespace Core.Application
             this.dbContext = dbContext;
         }
 
+        /// <summary>
+        /// Get all Commmunications in the DbContext
+        /// </summary>
+        /// <returns> List of Communications objects </returns>
         public List<Communication> GetCommunications()
         {
             return dbContext.Communications.ToList();
         }
 
+        /// <summary>
+        /// Get a single Communication with the given Id
+        /// </summary>
+        /// <param name="id"> Id of the Communication object to fetch </param>
+        /// <returns> Found Communication object</returns>
         public Communication GetCommunicationById(int id)
         {
-            //TODO: finish method
-            throw new NotImplementedException();
+            return dbContext.Communications.FirstOrDefault(c => c.Id == id);
         }
 
+        /// <summary>
+        /// Add the given Communication object to the database
+        /// </summary>
+        /// <param name="communication"> Communication object to insert </param>
         public void CreateCommunication(Communication communication)
         {
-            //TODO: finish method
-            throw new NotImplementedException();
+            communication.DateSent = new DateTimeOffset();
+            dbContext.Communications.Add(communication);
+            dbContext.SaveChanges();
         }
 
+        /// <summary>
+        /// Update an existing Communication with the given object
+        /// </summary>
+        /// <param name="communication"> Updated Communicaiton object </param>
         public void UpdateCommunication(Communication communication)
         {
-            //TODO: finish method
-            throw new NotImplementedException();
+            var com = dbContext.Communications.FirstOrDefault(c => c.Id == communication.Id);
+
+            // Update values
+            com.DateSent = new DateTimeOffset();
+            com.From = communication.From;
+            com.To = communication.To;
+            com.Subject = communication.Subject;
+            com.Message = communication.Message;
+            dbContext.SaveChanges();
         }
 
+        /// <summary>
+        /// Delete a single Communication with the given Id
+        /// </summary>
+        /// <param name="id"> Id of the Communication object to delete </param>
         public void DeleteCommunication(int id)
         {
-            //TODO: finish method
-            throw new NotImplementedException();
+            dbContext.Communications.Remove(GetCommunicationById(id));
+            dbContext.SaveChanges();
         }
     }
 }
